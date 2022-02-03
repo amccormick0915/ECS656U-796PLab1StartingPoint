4aca9377308d9ec8b84ffc1208aeb46b3cf70cab
AMcC <60145497+amccormick0915@users.noreply.github.com>
2022-02-03 17:25:52 +0000

    Update GrpcServer.java

--- src/main/java/server/GrpcServer.java
+++ src/main/java/server/GrpcServer.java
@@ -9,7 +9,7 @@ public class GrpcServer {
         Server server = ServerBuilder
           .forPort(8080)
           .addService(new MatrixServiceImpl()).build();
-	System.out.println("Starting server...");
+	System.out.println("Starting server...test");
         server.start();
 	System.out.println("Server started!");
         server.awaitTermination();
